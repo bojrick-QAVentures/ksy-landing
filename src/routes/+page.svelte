93a0626891936abfe3baf9596a203a93c9964f2b
pruthvi-bojrick <bojrick@pruthvi.group>
2023-06-11 02:25:06 -0500

    added carousel

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,27 @@
+<script>
+  import { Carousel } from 'flowbite-svelte'
+
+  // ./imageData/+server.js has the following
+   const images = [
+    {
+      id: 0,
+      name: "Cosmic timetraveler",
+      imgurl: "https://placehold.co/1600x1900",
+      attribution: "cosmic-timetraveler-pYyOZ8q7AII-unsplash.com",
+    },
+    {
+      id: 1,
+      name: "Cristina Gottardi",
+      imgurl: "https://placehold.co/1600x1900",
+      attribution: "cristina-gottardi-CSpjU6hYo_0-unsplash.com",
+    }
+  ];
+</script>
+
 <slot>
   <h1>Welcome to SvelteKit</h1>
-  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+  <div class="max-w-4xl">
+    <Carousel {images} />
+  </div>
 </slot>
